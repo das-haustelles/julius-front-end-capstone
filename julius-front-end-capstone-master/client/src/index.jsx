c09dc2c9f31d06887201791a2231801e2e3150d1
Julius Simtim <juliussimtim@gmail.com>
2019-04-10 16:39:28 -0700

    Clone repos instead of downloading them.

--- julius-front-end-capstone-master/client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import HeaderModule from './components/App.jsx'
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-
-ReactDOM.render(<HeaderModule />, document.getElementById('HeaderModule'));
--- stanley-front-end-capstone-master/client/src/index.jsx
+++
@@ -1,4 +0,0 @@
-// this is the entry point for webpack
-import SearchBar from './App.jsx'
-
-ReactDOM.render(<SearchBar/>, document.getElementById('SearchBar'))
